6faabf1ea2e311f74de77346c4ccac01544a02eb
doheek2 <doheek2@naver.com>
2022-09-28 01:41:12 +0900

    feat: route 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,18 @@ import styles from './Routes.module.scss'
 
 import Main from './Main'
 import SolveQuiz from './SolveQuiz'
+import ConfirmQuiz from './ConfirmQuiz'
 
 const App = () => {
   return (
-    <div className={styles.app}>
+    <main className={styles.app}>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='solveQuiz' element={<SolveQuiz />} />
+        <Route path='confirmQuiz' element={<ConfirmQuiz />} />
         <Route path='*' element={<div>404</div>} />
       </Routes>
-    </div>
+    </main>
   )
 }
 
