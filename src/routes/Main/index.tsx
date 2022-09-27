3e577b182d9535f71a6b085cf91db22569bd376c
doheek2 <doheek2@naver.com>
2022-09-28 01:40:05 +0900

    feat: 페이지 연결 및 디자인 추가

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,5 +1,31 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { FiArrowRight } from 'react-icons/fi'
+
+import styles from './main.module.scss'
+
 const Main = () => {
-  return <div>Main</div>
+  const navigate = useNavigate()
+  const startBtnClickHandler = () => navigate('/solveQuiz')
+
+  return (
+    <section className={styles.boxContainer}>
+      <article className={styles.headerContainer}>
+        <h3>quiztime</h3>
+        <p>퀴즈 10문제를 풀어보세요!</p>
+      </article>
+      <article className={styles.buttonContainer}>
+        <button type='button' onClick={startBtnClickHandler}>
+          시작하기
+          <div>
+            <FiArrowRight />
+          </div>
+        </button>
+        <Link to='/confirmQuiz' className={styles.link}>
+          퀴즈기록 확인하러 가기
+        </Link>
+      </article>
+    </section>
+  )
 }
 
 export default Main
