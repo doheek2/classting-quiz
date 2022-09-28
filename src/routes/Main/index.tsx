cb92a57615cb01df4bd6d8c3020ac4e42490cfa4
doheek2 <doheek2@naver.com>
2022-09-29 03:35:04 +0900

    feat: Box 컴포넌트 추가 및 link 문구 변경

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { FiArrowRight } from 'react-icons/fi'
 
+import Box from 'components/Box'
+
 import styles from './main.module.scss'
 
 const Main = () => {
@@ -8,23 +10,25 @@ const Main = () => {
   const startBtnClickHandler = () => navigate('/solveQuiz')
 
   return (
-    <section className={styles.boxContainer}>
-      <article className={styles.headerContainer}>
-        <h3>quiztime</h3>
-        <p>퀴즈 10문제를 풀어보세요!</p>
-      </article>
-      <article className={styles.buttonContainer}>
-        <button type='button' onClick={startBtnClickHandler}>
-          시작하기
-          <div>
-            <FiArrowRight />
-          </div>
-        </button>
-        <Link to='/confirmQuiz' className={styles.link}>
-          퀴즈기록 확인하러 가기
-        </Link>
-      </article>
-    </section>
+    <Box>
+      <>
+        <article className={styles.headerContainer}>
+          <h3>quiztime</h3>
+          <p>퀴즈 10문제를 풀어보세요!</p>
+        </article>
+        <article className={styles.buttonContainer}>
+          <button type='button' onClick={startBtnClickHandler}>
+            시작하기
+            <div>
+              <FiArrowRight />
+            </div>
+          </button>
+          <Link to='/confirmQuiz' className={styles.link}>
+            이전 퀴즈 보러 가기
+          </Link>
+        </article>
+      </>
+    </Box>
   )
 }
 
