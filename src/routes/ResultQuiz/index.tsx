4ff12a4afbdb0844c13d4f66274ba85606b750fc
doheek2 <doheek2@naver.com>
2022-10-01 23:50:56 +0900

    feat: localStorage 저장 기능, 그래프 애니메이션 기능, 시간 저장 state, 퀴즈 저장 button 추가

--- src/routes/ResultQuiz/index.tsx
+++ src/routes/ResultQuiz/index.tsx
@@ -1,18 +1,23 @@
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 import { VictoryPie } from 'victory'
+import store from 'storejs'
+import dayjs from 'dayjs'
 
-import { solveQuizListState, timerMinState, timerSecState } from 'store/atom'
+import { solveQuizListState, timeState } from 'store/atom'
 import Box from 'components/Box'
+import Label from 'components/Label'
 
 import styles from './resultQuiz.module.scss'
-import Label from 'components/Label'
 
 const ResultQuiz = () => {
   const navigate = useNavigate()
+  const [startAngle, setStartAngle] = useState(0)
+  const [endAngle, setEndAngle] = useState(0)
+
   const solveQuizList = useRecoilValue(solveQuizListState)
-  const min = useRecoilValue(timerMinState)
-  const sec = useRecoilValue(timerSecState)
+  const time = useRecoilValue(timeState)
 
   const correctList = solveQuizList.filter((v) => v.isCorrect === true)
 
@@ -21,15 +26,42 @@ const ResultQuiz = () => {
     { x: '오답 수', y: 10 - correctList.length },
   ]
 
+  useEffect(() => {
+    setTimeout(() => {
+      setStartAngle(90)
+      setEndAngle(450)
+    }, 100)
+  }, [])
+
   const homeBtnClickHandler = () => navigate('/')
+
+  const saveBtnClickHandler = () => {
+    const storage = store.get('quizList')
+
+    const storageObj = {
+      date: dayjs().format('YY-MM-DD'),
+      time: `${time[0]}:${time[1]}`,
+      data: solveQuizList,
+    }
+
+    if (storage === undefined) {
+      store.set('quizList', [{ id: 0, ...storageObj }])
+    } else {
+      const obj = { id: storage[storage.length - 1].id + 1, ...storageObj }
+      store.set('quizList', [...storage, obj])
+    }
+  }
 
   return (
     <Box>
       <article className={styles.resultContainer}>
         <main className={styles.chartContainer}>
           <VictoryPie
-            startAngle={90}
-            endAngle={450}
+            animate={{
+              duration: 2000,
+            }}
+            startAngle={startAngle}
+            endAngle={endAngle}
             data={data}
             colorScale={['#b7bbea', '#e4e9fe']}
             height={400}
@@ -41,13 +73,19 @@ const ResultQuiz = () => {
         </main>
         <footer className={styles.inforContainer}>
           <ul>
-            <Label color='#2a303f' name='소요 시간' text={`${min}분 ${sec}초`} />
+            <Label color='#2a303f' name='소요 시간' text={`${time[0]}분 ${time[1]}초`} />
+            <Label color='#2a303f' name='총 퀴즈 개수' text='10개' />
             <Label color='#b7bbea' name='정답 개수' text={`${data[0].y}개`} />
             <Label color='#e4e9fe' name='오답 개수' text={`${data[1].y}개`} />
           </ul>
-          <button type='button' onClick={homeBtnClickHandler}>
-            메인 화면으로 돌아가기
-          </button>
+          <div className={styles.btnContainer}>
+            <button type='button' className={styles.homeBtn} onClick={homeBtnClickHandler}>
+              메인 화면으로 돌아가기
+            </button>
+            <button type='button' className={styles.saveBtn} onClick={saveBtnClickHandler}>
+              퀴즈 저장하기
+            </button>
+          </div>
         </footer>
       </article>
     </Box>
