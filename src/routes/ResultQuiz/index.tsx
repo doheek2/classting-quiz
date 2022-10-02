9e4f21ce53939776d40bd3ea4d79bc5e5ff4efc2
doheek2 <doheek2@naver.com>
2022-10-02 23:33:24 +0900

    feat: 저장 버튼 클릭 시 모달 창 추가, disabled 변경

--- src/routes/ResultQuiz/index.tsx
+++ src/routes/ResultQuiz/index.tsx
@@ -4,10 +4,12 @@ import { useRecoilValue } from 'recoil'
 import { VictoryPie } from 'victory'
 import store from 'storejs'
 import dayjs from 'dayjs'
+import cx from 'classnames'
 
 import { solveQuizListState, timeState } from 'store/atom'
 import Box from 'components/Box'
 import Label from 'components/Label'
+import Modal from 'components/Modal'
 
 import styles from './resultQuiz.module.scss'
 
@@ -15,6 +17,8 @@ const ResultQuiz = () => {
   const navigate = useNavigate()
   const [startAngle, setStartAngle] = useState(0)
   const [endAngle, setEndAngle] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isSaveBtnDisabled, setIsSaveBtnDisabled] = useState(false)
 
   const solveQuizList = useRecoilValue(solveQuizListState)
   const time = useRecoilValue(timeState)
@@ -27,10 +31,8 @@ const ResultQuiz = () => {
   ]
 
   useEffect(() => {
-    setTimeout(() => {
-      setStartAngle(90)
-      setEndAngle(450)
-    }, 100)
+    setStartAngle(90)
+    setEndAngle(450)
   }, [])
 
   const homeBtnClickHandler = () => navigate('/')
@@ -50,44 +52,68 @@ const ResultQuiz = () => {
       const obj = { id: storage[storage.length - 1].id + 1, ...storageObj }
       store.set('quizList', [...storage, obj])
     }
+
+    setIsModalOpen(true)
+    setIsSaveBtnDisabled(true)
+  }
+
+  const modalCloseBtnClickHandler = () => {
+    setIsModalOpen(false)
   }
 
   return (
     <Box>
-      <article className={styles.resultContainer}>
-        <main className={styles.chartContainer}>
-          <VictoryPie
-            animate={{
-              duration: 2000,
-            }}
-            startAngle={startAngle}
-            endAngle={endAngle}
-            data={data}
-            colorScale={['#b7bbea', '#e4e9fe']}
-            height={400}
-            padAngle={1}
-            innerRadius={120}
-            cornerRadius={5}
-            labels={() => ''}
-          />
-        </main>
-        <footer className={styles.inforContainer}>
-          <ul>
-            <Label color='#2a303f' name='소요 시간' text={`${time[0]}분 ${time[1]}초`} />
-            <Label color='#2a303f' name='총 퀴즈 개수' text='10개' />
-            <Label color='#b7bbea' name='정답 개수' text={`${data[0].y}개`} />
-            <Label color='#e4e9fe' name='오답 개수' text={`${data[1].y}개`} />
-          </ul>
-          <div className={styles.btnContainer}>
-            <button type='button' className={styles.homeBtn} onClick={homeBtnClickHandler}>
-              메인 화면으로 돌아가기
-            </button>
-            <button type='button' className={styles.saveBtn} onClick={saveBtnClickHandler}>
-              퀴즈 저장하기
-            </button>
-          </div>
-        </footer>
-      </article>
+      <>
+        <article className={styles.resultContainer}>
+          <main className={styles.chartContainer}>
+            <VictoryPie
+              animate={{
+                duration: 2000,
+              }}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              data={data}
+              colorScale={['#b7bbea', '#e4e9fe']}
+              height={400}
+              padAngle={1}
+              innerRadius={120}
+              cornerRadius={5}
+              labels={() => ''}
+            />
+          </main>
+          <footer className={styles.inforContainer}>
+            <ul>
+              <Label color='#2a303f' name='소요 시간' text={`${time[0]}분 ${time[1]}초`} />
+              <Label color='#2a303f' name='총 퀴즈 개수' text='10개' />
+              <Label color='#b7bbea' name='정답 개수' text={`${data[0].y}개`} />
+              <Label color='#e4e9fe' name='오답 개수' text={`${data[1].y}개`} />
+            </ul>
+            <div className={styles.btnContainer}>
+              <button type='button' className={cx(styles.homeBtn, styles.btn)} onClick={homeBtnClickHandler}>
+                메인 화면으로 돌아가기
+              </button>
+              <button
+                type='button'
+                className={cx(styles.btn, styles.saveBtn, isSaveBtnDisabled && styles.disabledBtn)}
+                onClick={saveBtnClickHandler}
+                disabled={isSaveBtnDisabled}
+              >
+                퀴즈 저장하기
+              </button>
+            </div>
+          </footer>
+        </article>
+        {isModalOpen && (
+          <Modal setIsModalOpen={setIsModalOpen}>
+            <>
+              <p className={styles.modalText}>퀴즈를 저장하였습니다.</p>
+              <button type='button' className={styles.modalBtn} onClick={modalCloseBtnClickHandler}>
+                확인
+              </button>
+            </>
+          </Modal>
+        )}
+      </>
     </Box>
   )
 }
