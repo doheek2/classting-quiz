da7af77805d35dcbdf4bc645ba30f747a0af67e6
doheek2 <doheek2@naver.com>
2022-10-03 18:00:57 +0900

    fix: Router 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals'
 import './styles/index.scss'
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <Router>
         <Routes />
-      </BrowserRouter>
+      </Router>
     </RecoilRoot>
   </React.StrictMode>
 )
